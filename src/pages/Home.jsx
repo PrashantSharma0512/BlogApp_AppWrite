09c49b3efac693ebe489e92195f3bbbfaa3e9696
PrashantSharma0512 <prashantsharma0512@gmail.com>
2025-03-03 20:09:00 +0530

    fix bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -76,7 +76,7 @@ function Home() {
                 <Container>
                     {/* Responsive Grid for Post Cards */}
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                        {posts.map((post) => (
+                        {post.map((post) => (
                             <PostCard key={post.$id} {...post} />
                         ))}
                     </div>
